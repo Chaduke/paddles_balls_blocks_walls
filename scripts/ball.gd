477484b153f4d3a5a029c68d0b34a55603cb690c
Chad Dore <chaduke@gmail.com>
2024-11-23 15:32:19 -0600

    Added spare balls to right gutter area, added title label

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -14,8 +14,10 @@ func _on_body_exited(body):
 	# print("Exited " + body.name)	
 	if body.name=="paddle":
 		var diff = position.x - body.position.x
-		linear_velocity+=Vector3(diff,5,0)
+		linear_velocity.y *= 1.1
+		linear_velocity.x += diff * 2
 		bounce.play()
 	if body.name.begins_with("block"):
 		block.play()
 		body.queue_free()
+		linear_velocity.y*=1.1
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,19 @@
 extends Node3D
-
 @onready var paddle = $paddle
 var balls = []
 var ball_scene = preload("res://scenes/ball.tscn")
+var balls_left = 10
+@onready var stage = $stage
+@onready var camera = $Camera3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if camera.position.z < 0:
+		camera.position.z += 2	
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("spawn_ball"):
@@ -20,11 +25,14 @@ func _process(_delta):
 			ball.queue_free() 
 			balls.erase(ball) # Remove from the list break # Exit loop to prevent errors from modifying the array while iterating	
 
-func spawn_ball(): 	
-	var ball_instance = ball_scene.instantiate()
-	ball_instance.position = paddle.position + Vector3(0,5,0)
-	ball_instance.linear_velocity += Vector3(3,40,0)
-	balls.append(ball_instance) 
-	add_child(ball_instance)
-
-	
+func spawn_ball():	
+	var spare_balls = stage.get_node("spare_balls")
+	if spare_balls.get_child_count() > 0:
+		var spare_ball = spare_balls.get_child(0)
+		spare_ball.queue_free()
+		balls_left-=1
+		var ball_instance = ball_scene.instantiate()
+		ball_instance.position = paddle.position + Vector3(0,5,0)
+		ball_instance.linear_velocity += Vector3(3,40,0)
+		balls.append(ball_instance) 
+		add_child(ball_instance)
