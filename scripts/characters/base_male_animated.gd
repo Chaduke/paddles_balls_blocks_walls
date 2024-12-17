c4c52fd54719f3b97e594d642eba7fda9a721317
Chad Dore <chaduke@gmail.com>
2024-12-16 20:27:11 -0600

    Worked on Help Menu more, added levels 21-23

--- scripts/characters/base_male_animated.gd
+++ scripts/characters/base_male_animated.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -21,8 +20,8 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = -direction.x * SPEED
 		velocity.z = -direction.z * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
--- scripts/menus/help_menu.gd
+++ scripts/menus/help_menu.gd
@@ -8,7 +8,13 @@ func _ready() -> void:
 	
 func _process(_delta: float) -> void:
 	if active:
-		$block_yellow.rotate(Vector3(0.0,1.0,0.0),0.015)
-		$cracked_yellow.rotate(Vector3(0.0,1.0,0.0),0.02)
-		$block_blue.rotate(Vector3(0.0,1.0,0.0),0.025)
-		$cracked_blue.rotate(Vector3(0.0,1.0,0.0),0.03)
+		$yellow_blocks/block_yellow.rotate(Vector3(0.0,1.0,0.0),0.015)
+		$yellow_blocks/cracked_yellow.rotate(Vector3(0.0,1.0,0.0),0.02)
+		$blue_blocks/block_blue.rotate(Vector3(0.0,1.0,0.0),0.025)
+		$blue_blocks/cracked_blue.rotate(Vector3(0.0,1.0,0.0),0.03)
+		$pink_blocks/block_pink.rotate(Vector3(0.0,1.0,0.0),0.035)
+		$pink_blocks/cracked_pink.rotate(Vector3(0.0,1.0,0.0),0.04)
+		$clear_blocks/blue_ball_block.rotate(Vector3(0.0,1.0,0.0),0.05)
+		$clear_blocks/pink_ball_block.rotate(Vector3(0.0,1.0,0.0),0.055)
+		$metal_block.rotate(Vector3(0.0,1.0,0.0),0.05)
+  
